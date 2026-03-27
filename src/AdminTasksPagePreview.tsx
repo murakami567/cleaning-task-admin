3baa131869f866b9ce3579bf3f6954cb68e646da
murakami567 <murakami@gusk.jp>
2026-03-27 11:02:50 +0900

    Update AdminTasksPagePreview.tsx

--- src/AdminTasksPagePreview.tsx
+++ src/AdminTasksPagePreview.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
+import { sortTasksByPropertyOrder, sortByPropertyOrder } from "./utils/propertyOrder";
 
 /* =========================
  * Options
