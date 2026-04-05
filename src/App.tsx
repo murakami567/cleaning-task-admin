c4722c609fac8d2be5df7fda8046c1f6a90a042e
murakami567 <murakami@gusk.jp>
2026-04-05 10:08:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,74 @@ import FacilityManagementPage from "./FacilityManagementPage";
 import ShiftManagementPage from "./ShiftManagementPage";
 import ShiftBoardPage from "./ShiftBoardPage";
 
+import { AuthProvider } from "./context/AuthContext";
+import EmployeeProtectedRoute from "./routes/EmployeeProtectedRoute";
+
 import EmployeeLoginPage from "./pages/employee/EmployeeLoginPage";
 import EmployeeHomePage from "./pages/employee/EmployeeHomePage";
+import EmployeeTasksPage from "./pages/employee/EmployeeTasksPage";
+import EmployeeSchedulePage from "./pages/employee/EmployeeSchedulePage";
+import EmployeeWorklogPage from "./pages/employee/EmployeeWorklogPage";
+import EmployeeSettingsPage from "./pages/employee/EmployeeSettingsPage";
 
 export default function App() {
   return (
-    <Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin/tasks" replace />} />
 
-      {/* 管理画面 */}
-      <Route path="/admin/tasks" element={<AdminTasksPagePreview />} />
-      <Route path="/admin/properties" element={<PropertyManagementPage />} />
-      <Route path="/admin/openings" element={<OpeningManagementPage />} />
-      <Route path="/admin/facilities" element={<FacilityManagementPage />} />
-      <Route path="/admin/shifts" element={<ShiftManagementPage />} />
-      <Route path="/admin/shiftboard" element={<ShiftBoardPage />} />
+        <Route path="/admin/tasks" element={<AdminTasksPagePreview />} />
+        <Route path="/admin/properties" element={<PropertyManagementPage />} />
+        <Route path="/admin/openings" element={<OpeningManagementPage />} />
+        <Route path="/admin/facilities" element={<FacilityManagementPage />} />
+        <Route path="/admin/shifts" element={<ShiftManagementPage />} />
+        <Route path="/admin/shiftboard" element={<ShiftBoardPage />} />
 
-      {/* 一般画面 */}
-      <Route path="/employee/login" element={<EmployeeLoginPage />} />
-      <Route path="/employee/home" element={<EmployeeHomePage />} />
+        <Route path="/employee/login" element={<EmployeeLoginPage />} />
 
-      {/* デフォルト */}
-      <Route path="/" element={<Navigate to="/admin/tasks" replace />} />
+        <Route
+          path="/employee/home"
+          element={
+            <EmployeeProtectedRoute>
+              <EmployeeHomePage />
+            </EmployeeProtectedRoute>
+          }
+        />
+        <Route
+          path="/employee/tasks"
+          element={
+            <EmployeeProtectedRoute>
+              <EmployeeTasksPage />
+            </EmployeeProtectedRoute>
+          }
+        />
+        <Route
+          path="/employee/schedule"
+          element={
+            <EmployeeProtectedRoute>
+              <EmployeeSchedulePage />
+            </EmployeeProtectedRoute>
+          }
+        />
+        <Route
+          path="/employee/worklog"
+          element={
+            <EmployeeProtectedRoute>
+              <EmployeeWorklogPage />
+            </EmployeeProtectedRoute>
+          }
+        />
+        <Route
+          path="/employee/settings"
+          element={
+            <EmployeeProtectedRoute>
+              <EmployeeSettingsPage />
+            </EmployeeProtectedRoute>
+          }
+        />
 
-    </Routes>
+        <Route path="*" element={<Navigate to="/admin/tasks" replace />} />
+      </Routes>
+    </AuthProvider>
   );
 }
