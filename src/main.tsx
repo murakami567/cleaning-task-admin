ae7763d04885032648957555d120c1a99301d0c8
murakami567 <murakami@gusk.jp>
2026-06-14 19:03:54 +0900

    feat: enable missing clock-in highlight on admin home

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./property-sort-dnd";
+import "./admin-home-missing-clockin";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
